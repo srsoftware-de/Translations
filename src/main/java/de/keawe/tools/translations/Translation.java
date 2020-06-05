d4c53d26bb41e9daa772d6d96217387ebbe17087
Stephan Richter <stephan.richter.it@jena.de>
2020-06-05 18:47:49 +0200

    added javadoc comments

--- src/main/java/de/keawe/tools/translations/Translation.java
+++ src/main/java/de/keawe/tools/translations/Translation.java
@@ -8,13 +8,38 @@ import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 
+/**
+ * This class provides a helper to load translations from text files
+ * 
+ * @author Stephan Richter <stephan.richter.it@jena.de>
+ *
+ */
 public class Translation {
 	private static String root = System.getProperty("user.dir"); 
 	private static String locale = Locale.getDefault().getLanguage().toLowerCase();
 	private static Map<String, Map<String,String>> translations = new HashMap<String, Map<String,String>>();
 
+	/**
+	 * Get the translation for a text. Translations are loaded by clazz and default locale:
+	 * When you pass com.example.MyApp as class and your default locale is DE, the lib will search for a translation file named myApp.de.translation in your working directory.
+	 * @param clazz The class which uses the translations
+	 * @param text the text to be translated
+	 * @param fills fillers for {} stanzas, which are also translated
+	 * @return
+	 */
 	public static String get(Class<?> clazz, String text, String...fills) {
 		return getTranslation(getClassTranslations(clazz),text,fills);
+	}
+	
+	
+	/** Get the translation for a class, uses the context.getClass() method to call the above method
+	 * @param context
+	 * @param text
+	 * @param fills
+	 * @return
+	 */
+	public static String get(Object context, String text, String...fills) {
+		return get(context.getClass(),text,fills);
 	}
 
 	private static String getTranslation(Map<String, String> classTranslations, String text, String[] fills) {
@@ -84,9 +109,4 @@ public class Translation {
 		}
 		return null;
 	}
-
-	public static String get(Object context, String text, String...fills) {
-		return get(context.getClass(),text,fills);
-	}
-
 }
